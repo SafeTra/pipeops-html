cd13cc84a1b7d2d9841fa0bb5630905468720086
ObiFaith <obifth@gmail.com>
2024-06-10 20:58:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-600 font-bold underline">
+    <h1 className="text-3xl text-blue-600 font-bold underline">
       Hello world!
     </h1>
   )
